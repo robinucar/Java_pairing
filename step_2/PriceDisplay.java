f6ebc6be847b86f7f5ec09b35db73de8f638270a
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-12-14 11:56:38 +0000

    display itemPrice and itemName

---
+++ step_2/PriceDisplay.java
@@ -0,0 +1,9 @@
+package step_2;
+
+public class PriceDisplay {
+    public static void main(String[] args) {
+        String itemName = "Cofee Machine";
+        Double itemPrice = 15.99;
+        System.out.printf("The price for %s %.2f \n" , itemName , itemPrice);
+    }
+}
